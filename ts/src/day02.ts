25b00c2ce827113a20871ce942fcdf3adb1272b8
pubgrub2 <pubgrub2@fuechse.de>
2024-12-02 22:06:24 +0100

    day 02 part 1 working

---
+++ ts/src/day02.ts
@@ -0,0 +1,71 @@
+import { readFileSync } from 'node:fs'
+
+const day = "02"
+const test: boolean = false
+
+const testDataFile = "../../testdata/day" + day
+const dataFile = "../../data/day" + day
+
+let inputData: string[]
+
+if (test === true) {
+  inputData = readFileSync(testDataFile).toString().trimEnd().split("\n")
+  inputData = inputData.map(str => (str.trim()))
+}
+
+else {
+  inputData = readFileSync(dataFile).toString().trimEnd().split("\n")
+  inputData = inputData.map(str => (str.trim()))
+}
+
+function problem1(input: string[]): string{
+  const splitIntoNumbers = /(\d+)/g
+
+  let total: number = 0
+
+  for(let i=0; i<input.length; i++) {
+
+    let x: number[] = []
+    let y = input[i].match(splitIntoNumbers)
+
+    if (y !== null) {x = y.map(str => +str)}
+    else {console.log("continued"); continue}
+
+    let direction: number = 0
+
+    for(let l=0; l<x.length-1; l++) {
+      let current = x[l]
+      let next = x [l+1]
+
+      if ( current < next ) {
+        if (direction === 0) {direction = 1}
+        if (direction === 2) {break}
+        if ( next - current >= 1 && next - current <= 3 ) { if ( l === x.length-2 ) { total++; } }
+        else {break}
+      }
+
+      if ( current > next ) {
+        if (direction === 0) {direction = 2}
+        if (direction === 1) {break}
+        if ( current - next >= 1 && current - next <= 3 ) { if ( l === x.length-2 ) { total++; } }
+        else {break}
+      }
+
+      if ( current === next ) {break}
+
+    }
+  }
+
+  let output = total.toString()
+  return output
+}
+
+function problem2(input: string[]): string{
+
+
+  let output = ""
+  return output
+}
+
+console.log("Part 1:\n" + problem1(inputData))
+console.log("Part 2:\n" + problem2(inputData))
